7c718db25a2e10a6db8c8d75099d45f769610a64
BradleyParkin <bradbusiness@hotmail.com>
2023-01-20 15:23:31 +0000

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const APR_KEY = "rgcsj59Kr3dns1J8FdMvd6AbSyk"
+const API_URL = "https://ci-jshint.herokuapp.com/api"
+const resultsModal = new boostrap.Modal(document.getElementById("resultsModal"));
